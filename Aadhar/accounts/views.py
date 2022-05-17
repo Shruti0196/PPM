51e7de62b75e4917944b6c55b1899abf3d0688ed
Shruti Prasad <80119277+Shruti0196@users.noreply.github.com>
2022-05-17 12:21:26 +0530

    Update views.py

--- Aadhar/accounts/views.py
+++ Aadhar/accounts/views.py
@@ -15,14 +15,9 @@ from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
 from django.contrib.auth.decorators import login_required
 from .models import *
-# from django.conf import settings
-# # User = settings.AUTH_USER_MODEL
+
 from .serializers import UserSerializer,AddressSerializer,QualificationSerializer,BankSerializer,personaldetailsSerializer,PastJobExpSerializer,AadharSerializer
-# # Create your views here.
-# @csrf_exempt
- # @method_decorator(csrf_protect)
-# @api_view(['POST'])
-# def registeruser(self,request,*args,**kwargs):
+
 class RegisterUser(APIView):
     def post(self,request):
         serializer=UserSerializer(data=request.data)
@@ -67,17 +62,7 @@ class StaffUser(APIView):
 
         return Response({'status':200,'payload':serializer.data,'token':str(token_obj),'message':'valid'})
 
-# @api_view(['POST'])
-# def stafflogin(request):
-#     serializer=UserSerializer(request.data)
-#     username=serializer.data['username']
-#     password=serializer.data['password']
-#     user = authenticate(request,username=username,password=password)
-#     if user is not None:
-#             login(request,user)
-#             return Response('logged in')    
 
-# @login_required(login_url='login')
 from .decorators import unauthenticated_user,allowed_users
 
 #Aadhar
@@ -460,154 +445,4 @@ def aadharinactive(request):
         response_dict={"data":serializer.data}
         return Response(response_dict)  
 
-        # return Response('got')
-# print(details.address.all())
-        # print(details.address.get().id)
-        # # serializer=[]
-        # # for i in details.address.all():
-        # #    id=i.get().id
-        # # add_data=Address.objects.filter(id=(details['address']))
-        # serializer=AddressSerializer(add_data,many=True,context={"request":request})
-# from django.contrib import messages
-
-
-
-# # @allowed_users(allowed_roles=['Admin'])
-# def homepage(request):
-#     return render(request, 'homepage.html')
-# # def showform(request):
-# #     form=TodoForm()
-# #     return render(request,'index.html',{'form':form})
-# # from django.urls import reverse_lazy
-# from django.views.generic.edit import CreateView
-
-# from .forms import UserCreationForm
-# from django.views.decorators.csrf import csrf_exempt
-# from rest_framework.views import APIView
-
-# def homepage(request):
-#     return render(request, 'homepage.html')
-
-# # @csrf_exempt
-# @unauthenticated_user
-# def staffsignup(request):
-#     if request.method == 'POST':
-#             form=UserCreationForm(request.POST)
-#             if form.is_valid():
-#                 user=form.save()
-#                 # username=form.cleaned_data.get('username')
-#                 group=Group.objects.get(name='staff')
-#                 user.groups.add(group)
-#                 # messages.success(request,'Account was created for '+username)
-#                 return redirect("stafflogin")
-#     if request.method == 'GET':
-#         form=UserCreationForm()
-#         return render(request,'signup.html',{'form':form})
-
-# # @unauthenticated_user
-# # def managersignup(request):
-# #         form=MyUserCreationForm()
-# #         if request.method =='POST':
-# #             form=MyUserCreationForm(request.POST)
-# #             if form.is_valid():
-# #                 user=form.save()
-# #                 username=form.cleaned_data.get('username')
-# #                 group=Group.objects.get(name='Manager')
-# #                 user.groups.add(group)
-# #                 # messages.success(request,'Account was created for '+username)
-# #                 return redirect("managerlogin")
-# #         return render(request,'signup.html',{'form':form})
-
-# # @csrf_exempt
-# @unauthenticated_user
-# def stafflogin(request):
-#     if request.method == 'POST':
-#             email = request.POST.get('email')
-#             password = request.POST.get('password')
-#             user = authenticate(request,email=email,password=password)
-#             # if auth.authenticate(username=username, password=password):
-#             if user is not None:
-#                 login(request,user)
-#                 return redirect('/')
-#             return redirect('stafflogin')
-
-#     if request.method == 'GET':
-#             return render(request, 'login.html')
-#             # return HttpResponse('Login')+
-
-# # @unauthenticated_user
-# # def managerlogin(request):
-    
-# #         if request.method == 'POST':
-# #             username = request.POST.get('username')
-# #             password = request.POST.get('password')
-# #             user = authenticate(request,username=username,password=password)
-# #             # if auth.authenticate(username=username, password=password):
-# #             if user is not None:
-# #                 login(request,user)
-# #                 return redirect('addform')
-# #             return redirect('userlogin')
-
-# #         if request.method == 'GET':
-# #             return render(request, 'login.html')
-
-# # def userlogout(request):
-# #     logout(request)
-# #     return redirect('/')
-
-# # @login_required(login_url='login')
-# # def addform(request):
-# #     form=TodoForm()
-# #     if request.method=='POST':
-# #         # print("Post:",request.POST)
-# #         form=TodoForm(request.POST)
-# #         if form.is_valid():
-# #             form.save()
-# #             return redirect('/details')
-# #     if request.method=='GET':        
-# #         return render(request,'index.html',{'form':form}) 
-
-# # @login_required(login_url='login')
-# # @allowed_users(allowed_roles=['Admin'])
-# # def adddetailsform(request):
-# #     form=DetailsForm()
-# #     if request.method=='POST':
-# #         form=DetailsForm(request.POST)
-# #         if form.is_valid():
-# #             form.save()
-            
-# #             return redirect('addform')
-# #     return render(request,'index.html',{'form':form})
-
-# # @login_required(login_url='login')
-
-# # def updateworkdetails(request,Work1):
-# #     # work=WorkDetails.objects.get(Q(WorkDetails__Work__Work=pk))
-# #     # work=WorkDetails.objects.filter(Work=work1)
-# #     # print(work)
-# #     work=WorkDetails.objects.get(Work=Work1)
-# #     print(work)
-# #     form=DetailsForm(instance=work)
-# #     if request.method=='POST':
-# #         form=DetailsForm(request.POST,instance=work)
-# #         if form.is_valid():
-# #             form.save()
-# #             return redirect('/display')
-# #     return render(request,'index.html',{'form':form})
-
-# # @login_required(login_url='login')
-# # def Display(request):
-# #     Todo=WorkDetails.objects.all()
-# #     return render(request,'Display.html',{'Todo':Todo})
-
-# # @login_required(login_url='login')
-# # def deletework(request,pk):
-# #         work=WorkDetails.objects.get(desc=pk)
-# #         work.delete()
-# #         return redirect('/display')
-
-# # accounts/views.py
-
-
-     
-
+      
